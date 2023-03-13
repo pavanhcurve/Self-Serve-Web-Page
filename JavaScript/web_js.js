31ae93b80dba6955eb6e2f393632e07c9748b7ae
hiteshhcurve <hitesh.shinde@hockeycurve.com>
2023-03-13 20:04:04 +0530

    created new cta

--- JavaScript/web_js.js
+++ JavaScript/web_js.js
@@ -1,3 +1,44 @@
 const menu = () => {
     document.getElementById("check").checked = false;
 }
+
+gsap.from(".title", {
+    top: -100,
+    stagger: 0.05,
+    delay: 1,
+    duration: 5,
+    ease: "power1"
+});
+
+gsap.from(".icon", {
+    scale: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: 0.8,
+    ease: "bounce"
+});
+
+gsap.from(".step", {
+    scale: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: 0.6,
+    ease: "bounce"
+});
+
+gsap.from(".cta-btn", {
+    scale: 0,
+    opacity: 0,
+    stagger: 0.05,
+    delay: 2,
+    duration: 0.3,
+    ease: "power1"
+});
+
+gsap.from(".cta-text", {
+    opacity: 0,
+    stagger: 0.05,
+    delay: 3,
+    duration: 0.6,
+    ease: "power0"
+});
