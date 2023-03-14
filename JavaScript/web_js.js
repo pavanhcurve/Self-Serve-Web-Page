0cd55002dc8cab4a6faa4b90e843bd83d0c94f58
pavanhcurve <pavan.vichare@hockeycurve.com>
2023-03-14 11:34:16 +0530

    2 sections responsive, make changes in swiper, self serve video

--- JavaScript/web_js.js
+++ JavaScript/web_js.js
@@ -3,6 +3,41 @@ const menu = () => {
     document.getElementById("check").checked = false;
 }
 
+// modal video
+// Get the modal and video elements
+var modal = document.getElementById("video-modal");
+var video = document.getElementById("modal-video");
+
+// Get the button that opens the modal
+var btn = document.getElementById("onplay");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  video.classList.add("scale-video"); 
+  video.play();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+//   video.classList.remove("scale-video");
+  video.pause();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    // video.classList.remove("scale-video");
+    video.pause();
+  }
+}
+
+
 // swiper js
 
 var swiper = new Swiper(".slider", {
