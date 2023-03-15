5684c6161171e507ae9bfc7cc79ee465b2381046
pavanhcurve <pavan.vichare@hockeycurve.com>
2023-03-15 15:16:32 +0530

    latest

--- JavaScript/web_js.js
+++ JavaScript/web_js.js
@@ -1,38 +1,31 @@
+
 // hamberger
 const menu = () => {
     document.getElementById("check").checked = false;
 }
 
-// modal video
-// Get the modal and video elements
+// video modal
 var modal = document.getElementById("video-modal");
 var video = document.getElementById("modal-video");
 
-// Get the button that opens the modal
 var btn = document.getElementById("onplay");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
   video.classList.add("scale-video"); 
   video.play();
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-//   video.classList.remove("scale-video");
   video.pause();
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    // video.classList.remove("scale-video");
     video.pause();
   }
 }
@@ -92,26 +85,68 @@ $(window).scroll(function () {
         });
         a = 1;
     }
+    var opc = $('#banner').offset().top - window.innerHeight;
+    if ($(window).scrollTop() > opc) {
+        console.log("hello");
+    }
+    
 
 });
 
 
 // testimony
-var swiper = new Swiper(".test_mony_Swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+// var swiper = new Swiper(".test_mony_Swiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//         rotate: 0,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
+
+// carousel 3d
+new Vue({
+    el: '#carousel3d',
+    data: {
+        slides: 7
     },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+    components: {
+        'carousel-3d': window['carousel-3d'].Carousel3d,
+        'slide': window['carousel-3d'].Slide
+    }
+})
+
+
+
+
+// animations
+function vidBounce() {var tll =new TimelineMax({repeat:0,repeatDelay:0,delay:0});
+tll.fromTo("#vidcontent",0.3,{x:30,scale:0.7,ease:Power0.easeBounce},{x:0,scale:1,ease:Power0.easeBounce});
+tll.to("#vidcontent",0.3,{x:30,scale:0.7,ease:Power0.easeBounce},);
+tll.to("#vidcontent",0.4,{x:0,scale:1,ease:Power0.easeBounce});
+}vidBounce();
+
+
+// image change on media query
+var chng = document.getElementById("change");
+function chngImage(imgchange){
+    if(imgchange.matches){
+        chng.src = "/Icons/laptopmob.gif";
+    }else{
+        chng.src = "/Icons/dco1.gif";
+    }
+}
+var imgchange = window.matchMedia("(max-width: 750px)");
+chngImage(imgchange);
+imgchange.addEventListener(chngImage);
+
 
 
