222676cbee04e32edaef9fcaee8c4f8b8c2c88ac
hiteshhcurve <hitesh.shinde@hockeycurve.com>
2023-03-16 16:48:23 +0530

    fixed testimonial carousel

--- JavaScript/web_js.js
+++ JavaScript/web_js.js
@@ -135,11 +135,47 @@ new Vue({
     data: {
         slides: 7
     },
+    dots: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    pauseOnHover: true,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+
+    ],
     components: {
         'carousel-3d': window['carousel-3d'].Carousel3d,
         'slide': window['carousel-3d'].Slide
     }
-})
+});
+
+// var imgchange = window.matchMedia("(max-width: 750px)");
+// if (window.matchMedia("(max-width: 750px)"))
 
 //start added by Chase
 //   var a = document.getElementsByTagName("a");
