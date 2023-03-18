1de31148fd4b90b9b7cc129af772673eee6cc06c
hiteshhcurve <hitesh.shinde@hockeycurve.com>
2023-03-18 17:30:34 +0530

    improved responsiveness for big screens

--- JavaScript/web_js.js
+++ JavaScript/web_js.js
@@ -1,7 +1,7 @@
 
 // hamberger
 const menu = () => {
-    document.getElementById("check").checked = false;
+  document.getElementById("check").checked = false;
 }
 
 // video modal
@@ -13,75 +13,76 @@ var btn = document.getElementById("onplay");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
-    modal.style.display = "block";
-    video.classList.add("scale-video");
-    video.play();
+  modal.style.display = "block";
+  video.classList.add("scale-video");
+  video.play();
 }
 
 span.onclick = function () {
-    modal.style.display = "none";
-    video.pause();
+  modal.style.display = "none";
+  video.pause();
 }
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        video.pause();
-    }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//     video.pause();
+//   }
+// }
+
 
 //swiper first
 var swiper = new Swiper(".swiper-container", {
-    slidesPerView: 1,
-    initialSlide: 0,
-    spaceBetween: 10,
-    loop: true,
-    breakpoints: {
-        350: {
-            slidesPerView: 1,
-            spaceBetween: 50
-        },
-        480: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        580: {
-            slidesPerView: 2,
-            spaceBetween: 40
-        },
-        646: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        900: {
-            slidesPerView: 2,
-            spaceBetween: 5
-            // width: 1000,
-        },
-        1000: {
-            slidesPerView: 3,
-            spaceBetween: 20
-            // width: 1000,
-        },
-        2000: {
-            slidesPerView: 5,
-            spaceBetween: 20
-        }
+  slidesPerView: 1,
+  initialSlide: 0,
+  spaceBetween: 10,
+  loop: true,
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 50
     },
-    // Optional parameters
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-    navigation: {
-        nextEl: ".swiper-left-btn",
-        prevEl: ".swiper-right-btn"
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 40
     },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: true,
-        reverseDirection: false
+    646: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 5
+      // width: 1000,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20
+      // width: 1000,
+    },
+    1700: {
+      slidesPerView: 4,
+      spaceBetween: 20
     }
+  },
+  // Optional parameters
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-left-btn",
+    prevEl: ".swiper-right-btn"
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+    reverseDirection: false
+  }
 });
 
 
@@ -89,37 +90,37 @@ var swiper = new Swiper(".swiper-container", {
 var a = 0;
 $(window).scroll(function () {
 
-    var oTop = $('#counter').offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-        $('.counter-value').each(function () {
-            var $this = $(this),
-                countTo = $this.attr('data-count');
-            $({
-                countNum: $this.text()
-            }).animate({
-                countNum: countTo
-            },
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum: countTo
+      },
 
-                {
+        {
 
-                    duration: 5000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.floor(this.countNum));
-                    },
-                    complete: function () {
-                        $this.text(this.countNum);
-                        //alert('finished');
-                    }
+          duration: 5000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
 
-                });
         });
-        a = 1;
-    }
-    var opc = $('#banner').offset().top - window.innerHeight;
-    if ($(window).scrollTop() > opc) {
-        console.log("hello");
-    }
+    });
+    a = 1;
+  }
+  var opc = $('#banner').offset().top - window.innerHeight;
+  if ($(window).scrollTop() > opc) {
+    console.log("hello");
+  }
 
 
 });
@@ -137,40 +138,78 @@ const usnum = document.getElementById("usnum");
 const err = document.getElementById("err");
 
 const valid = () => {
-    if (usname.value == "" || usemail.value == "" || usnum.value == "" || agname.value == "") {
-        usname.style.border = "3px solid red";
-        usemail.style.border = "3px solid red";
-        agname.style.border = "3px solid red";
-        usnum.style.border = "3px solid red";
-        footermodal.style.display = "none";
-        err.style.visibility = "visible";
-        setTimeout(function () {
-            usname.style.border = "3px solid transparent";
-            usemail.style.border = "3px solid transparent";
-            agname.style.border = "3px solid transparent";
-            usnum.style.border = "3px solid transparent";
-            err.style.visibility = "hidden";
-        }, 2000);
-    } else {
-        footermodal.style.display = "block";
-        anscale.classList.add("scalemore");
-        showname.innerHTML = usname.value;
-        setTimeout(function () {
-            footermodal.style.display = "none";
-        }, 5000);
-    }
+  if (usname.value == "") {
+    usname.style.border = "3px solid red";
+    footermodal.style.display = "none";
+    err.style.visibility = "visible";
+    setTimeout(function () {
+      usname.style.border = "3px solid transparent";
+      usemail.style.border = "3px solid transparent";
+      agname.style.border = "3px solid transparent";
+      usnum.style.border = "3px solid transparent";
+      err.style.visibility = "hidden";
+    }, 2000);
+  }
+  if (usemail.value == "") {
+    usemail.style.border = "3px solid red";
+    footermodal.style.display = "none";
+    err.style.visibility = "visible";
+    setTimeout(function () {
+      usname.style.border = "3px solid transparent";
+      usemail.style.border = "3px solid transparent";
+      agname.style.border = "3px solid transparent";
+      usnum.style.border = "3px solid transparent";
+      err.style.visibility = "hidden";
+    }, 2000);
+  }
+  if (agname.value == "") {
+    agname.style.border = "3px solid red";
+    footermodal.style.display = "none";
+    err.style.visibility = "visible";
+    setTimeout(function () {
+      usname.style.border = "3px solid transparent";
+      usemail.style.border = "3px solid transparent";
+      agname.style.border = "3px solid transparent";
+      usnum.style.border = "3px solid transparent";
+      err.style.visibility = "hidden";
+    }, 2000);
+  }
+  if (usnum.value == "") {
+    usnum.style.border = "3px solid red";
+    footermodal.style.display = "none";
+    err.style.visibility = "visible";
+    setTimeout(function () {
+      usname.style.border = "3px solid transparent";
+      usemail.style.border = "3px solid transparent";
+      agname.style.border = "3px solid transparent";
+      usnum.style.border = "3px solid transparent";
+      err.style.visibility = "hidden";
+    }, 2000);
+  }
+  if (usname.value !== "" && agname.value !== "" && usemail.value !== "" && usnum.value !== "") {
+    footermodal.style.display = "block";
+    anscale.classList.add("scalemore");
+    showname.innerHTML = usname.value;
+    setTimeout(function () {
+      footermodal.style.display = "none";
+    }, 5000);
+  } else {
+    footermodal.style.display = "none";
+  }
 }
-
 
 
 closee.onclick = function () {
-    footermodal.style.display = "none";
+  footermodal.style.display = "none";
 }
 
 window.onclick = function (event) {
-    if (event.target == footermodal) {
-        footermodal.style.display = "none";
-    }
+  if (event.target == footermodal) {
+    footermodal.style.display = "none";
+  } else if (event.target == modal) {
+    modal.style.display = "none";
+    video.pause();
+  }
 }
 
 // testimony
@@ -194,47 +233,47 @@ window.onclick = function (event) {
 //carousel last
 
 new Vue({
-    el: '#carousel3d',
-    data: {
-        slides: 7
+  el: '#carousel3d',
+  data: {
+    slides: 7
+  },
+  dots: false,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  pauseOnHover: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+      }
     },
-    dots: false,
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    autoplay: true,
-    autoplaySpeed: 2000,
-
-    pauseOnHover: true,
-
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 5,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }
-
-    ],
-    components: {
-        'carousel-3d': window['carousel-3d'].Carousel3d,
-        'slide': window['carousel-3d'].Slide
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
     }
+
+  ],
+  components: {
+    'carousel-3d': window['carousel-3d'].Carousel3d,
+    'slide': window['carousel-3d'].Slide
+  }
 });
 
 // new Vue({
@@ -359,10 +398,10 @@ new Vue({
 
 // animations
 function vidBounce() {
-    var tll = new TimelineMax({ repeat: 0, repeatDelay: 0, delay: 0 });
-    tll.fromTo("#vidcontent", 0.5, { x: 300, scale: 0, ease: Power0.easePower0 }, { x: 0, scale: 1, ease: Power0.easePower0 });
-    tll.to("#vidcontent", 0.2, { x: 100, scale: 0.65, ease: Power0.easePower0 });
-    tll.to("#vidcontent", 0.3, { x: 0, scale: 1, ease: Power0.easePower0 });
+  var tll = new TimelineMax({ repeat: 0, repeatDelay: 0, delay: 0 });
+  tll.fromTo("#vidcontent", 0.5, { x: 300, scale: 0, ease: Power0.easePower0 }, { x: 0, scale: 1, ease: Power0.easePower0 });
+  tll.to("#vidcontent", 0.2, { x: 100, scale: 0.65, ease: Power0.easePower0 });
+  tll.to("#vidcontent", 0.3, { x: 0, scale: 1, ease: Power0.easePower0 });
 } vidBounce();
 
 
@@ -420,11 +459,11 @@ function vidBounce() {
 // });
 
 gsap.from(".icon1,.icon3", {
-    scale: 0,
-    repeat: -1,
-    repeatDelay: 1,
-    duration: 0.8,
-    ease: "bounce"
+  scale: 0,
+  repeat: -1,
+  repeatDelay: 1,
+  duration: 0.8,
+  ease: "bounce"
 });
 
 
@@ -438,12 +477,16 @@ document.getElementById("year").innerHTML = current;
 // image change on media query
 var chng = document.getElementById("change");
 function chngImage(imgchange) {
-    if (imgchange.matches) {
-        chng.src = "https://s.hcurvecdn.com/selfserve/icons/fulllaptop.gif";
-    } else {
-        chng.src = "https://s.hcurvecdn.com/selfserve/icons/halflaptop.gif";
-    }
+  if (imgchange.matches) {
+    chng.src = "https://s.hcurvecdn.com/selfserve/icons/fulllaptop.gif";
+  } else {
+    chng.src = "https://s.hcurvecdn.com/selfserve/icons/halflaptop.gif";
+  }
 }
 var imgchange = window.matchMedia("(max-width: 750px)");
 chngImage(imgchange);
 imgchange.addEventListener(chngImage);
+
+
+
+
